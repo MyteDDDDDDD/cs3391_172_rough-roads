f18d1c79e6264a6081ea54c18dee8e994f851493
MyteD <elvisyuan6988@gmail.com>
2017-03-03 18:43:26 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,21 +3,33 @@
 
 using namespace std;
 
-int cal(int *a[],int j) {	//Dijkstra’s algorithm
+
+int cal(int *a[], int j) {	//Dijkstra’s algorithm
 	int *d;
 	d = new int[j];//distance from source to certain junctions
-	bool *q;
-	q = new bool[j]; 
-
-	for (int i = 0; i < j; i++) {//initial
-		d[i] = 20 * j;
-		q[i] = false;
-	}
-	int parent = 0;
-	int count=0;
+	int *p;
+	p = new int[j];// for the nodes on th shortest path from 0 junctions to n-1 junction, p[n] is its parent node 
 	
-	while (q[count] != true) {
 
+	for (int i = 0; i < j; i++) {
+		d[i] = 20 * j;//initial all d to 20*j ( go through all roads and each road with maximum length)
+		d[0] = 0;
+	}
+	
+	int count = 0;
+	while (count<j) {
+		for (int i = 0;i < j;i++) { // the road whose source is count-th junction and destination is i-th junction
+			if (a[count][i]>1) {// there is the road whose destination junction is the count-th junction
+		
+					if (d[i] > d[count] + a[count][i]) {
+						d[i] = d[count] + a[count][i];
+						p[i] = count;
+					
+				}
+				
+			}	
+		}
+		count++;
 	}
 
 	return d[j - 1];
@@ -32,18 +44,18 @@ int main() {
 	while (!cin.eof()) {
 		cin >> r;
 
-		int **roads=new int*[j];// a j*j matrix stors all roads;
+		int **roads = new int*[j];// a j*j matrix stors all roads;
 		for (int i = 0; i < j; i++) {
 			roads[i] = new int[j];
 		}
-		
-		int tempa, tempb,templ;
+
+		int tempa, tempb, templ;
 		for (int i = 0; i < r; i++) {
 			cin >> tempa >> tempb >> templ;
 			roads[tempa][tempb] = templ;
 			roads[tempb][tempa] = templ;
 		}
-		c.push(cal(roads,j));
+		c.push(cal(roads, j));
 		cin >> j;
 	}
 
